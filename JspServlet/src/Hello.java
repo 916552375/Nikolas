f075c6dc9c1ba8cb8716fb0ae04b4cb977f5b2a3
916552375 <916552375@qq.com>
2018-07-30 11:45:28 +0800

    "commit JspServlet Hello"

---
+++ JspServlet/src/Hello.java
@@ -0,0 +1,38 @@
+
+ /*
+ * @Author: Administrator
+ * @Despcription:
+ * @Date: Created in 2018/7/30 9:39
+ * @Modified by:
+ */
+
+ import javax.servlet.ServletException;
+ import javax.servlet.annotation.WebServlet;
+ import javax.servlet.http.HttpServlet;
+ import javax.servlet.http.HttpServletRequest;
+ import javax.servlet.http.HttpServletResponse;
+ import java.io.IOException;
+ import java.io.PrintWriter;
+
+ @WebServlet("/index")
+ public class Hello extends HttpServlet {
+
+     private String message;
+
+     @Override
+     public void init()throws ServletException{
+         message = "Nice to meet you .This message is from servlet!";
+     }
+     @Override
+     protected void doGet(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException{
+         //设置响应内容类型
+         resp.setContentType("text/html");
+         //设置逻辑实现
+         PrintWriter out = resp.getWriter();  out.println(" "+ message +" ");
+     }
+     @Override
+     public void destroy(){
+         super.destroy();
+     }
+
+ }
